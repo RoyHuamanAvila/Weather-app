7f609e0dc2fc420320c844d002dced47d6709714
Roy <rhavila789@gmail.com>
2022-11-06 08:30:41 -0500

    feat: fix responsive design

--- src/components/Highlights/index.tsx
+++ src/components/Highlights/index.tsx
@@ -5,19 +5,21 @@ const Highlights = () => {
     return(
         <div className='highligths-container'>
             <p>Today's Highlights</p>
-            <HighlightCard title='Wind status' highlight={7} unit='mph'>
-                <p>WSW</p>
-            </HighlightCard> 
-            <HighlightCard title='Humidity' highlight={84} unit='%'>
-                <div className='measurer'>
-                    <span>0</span><span>50</span><span>100</span>
-                </div>
-                <div className="bar-border">                    
-                    <div className="bar-progress" style={{width: '50%'}}></div>
-                </div>
-            </HighlightCard>
-            <HighlightCard title='Visibility' highlight='6,4' unit=' miles' />
-            <HighlightCard title='Air Pressure' highlight={998} unit=' mb' />
+            <div className="highlights-list">
+                <HighlightCard title='Wind status' highlight={7} unit='mph'>
+                    <p>WSW</p>
+                </HighlightCard> 
+                <HighlightCard title='Humidity' highlight={84} unit='%'>
+                    <div className='measurer'>
+                        <span>0</span><span>50</span><span>100</span>
+                    </div>
+                    <div className="bar-border">                    
+                        <div className="bar-progress" style={{width: '84%'}}></div>
+                    </div>
+                </HighlightCard>
+                <HighlightCard title='Visibility' highlight='6,4' unit=' miles' />
+                <HighlightCard title='Air Pressure' highlight={998} unit=' mb' />
+            </div>
         </div>
     )
 }
